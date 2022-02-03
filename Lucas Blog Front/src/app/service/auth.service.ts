25ef683f629bddbe6c9414ff1a09b42246729212
Lucas Melo <93784723+LucasCostaMelo@users.noreply.github.com>
2022-02-03 11:18:21 -0300

    BlogPessoal

---
+++ Lucas Blog Front/src/app/service/auth.service.ts
@@ -0,0 +1,50 @@
+import { environment } from './../../environments/environment.prod';
+import { UserLogin } from './../model/UserLogin';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(userLogin: UserLogin): Observable<UserLogin>{
+    return this.http.post<UserLogin>('https://blogpessoallucasmelo.herokuapp.com/usuarios/logar', userLogin)
+  }
+
+  cadastrar(user: User): Observable<User>{
+    return this.http.post<User>('https://blogpessoallucasmelo.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogpessoallucasmelo.herokuapp.com/usuarios/${id}`)
+  }
+
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
+  }
+
+  adm(){
+    let ok: boolean = false
+
+    if (environment.tipo == 'adm'){
+      ok = true
+    }
+
+    return ok
+  }
+
+}
